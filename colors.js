cd4ac26ce2ecd3aa03a6a7953c2ad8fdcaf6d238
Eddie Lindsey <lindseye@01245-DT-LindseyE.local>
2018-12-11 14:34:19 +0000

    Add colurs.js

---
+++ colors.js
@@ -0,0 +1,69 @@
+const lightblue = [
+    "aqua",
+    "aquamarine",
+    "cyan",
+    "darkturquoise",
+]
+
+const blue = [
+    "blue",
+    "cadetblue",
+    "cornflowerblue",
+]
+
+const lime = [
+    "chartreuse",
+    "greenyellow",
+    "lawngreen",
+]
+
+const red = [
+    "brown",
+    "crimson",
+    "darkred",
+]
+
+const green = [
+    "darkgreen",
+    "darkolivegreen",
+    "darkslategrey",
+]
+
+const yellow = [
+    "antiquewhite",
+    "beige",
+    "bisque",
+]
+
+const orange = [
+    "darkgoldenrod",
+    "darkorange",
+    "goldenrod",
+]
+
+const purple = [
+    "blueviolet",
+    "darkmagenta",
+    "darkorchid",
+]
+
+const pink = [
+    "deeppink",
+    "fuchsia",
+    "hotpink",
+]
+
+const brown = [
+    "brown",
+    "chocolate",
+    "darkgoldenrod",
+]
+
+const grey = [
+    "darkgrey",
+    "dimgrey",
+    "grey",
+]
+
+const colors = [lightblue,blue,lime,red,green,yellow,orange,purple,pink,brown,grey];
+const lightColors = [yellow,lightblue,pink,lime];
